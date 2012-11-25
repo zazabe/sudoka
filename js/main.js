802a7da56f682c275dff3fdfcd18f14a203cbf50
Michel Meyer <michel@zazabe.fr>
2012-11-25 11:56:05 +0700

    first sudoku solver version - no brute force algo yet

--- js/main.js
+++ js/main.js
@@ -6,14 +6,11 @@ require.config({
     jquery: 'libs/jquery/jquery-min',
     underscore: 'libs/underscore/underscore-min', // https://github.com/amdjs
     backbone: 'libs/backbone/backbone-min', // https://github.com/amdjs
-    sinon: 'libs/sinon/sinon.js',
 
     // Require.js plugins
     text: 'libs/require/text',
     order: 'libs/require/order',
 
-    // Just a short cut so we can put our html outside the js dir
-    // When you have HTML/CSS designers this aids in keeping them out of the js directory
     templates: '../templates'
   }
 
@@ -21,12 +18,20 @@ require.config({
 
 // Let's kick off the application
 
+var sodoku = null;
+
 require([
-  'views/app',
-  'router',
-  'vm'
-], function(AppView, Router, Vm){
-  var appView = Vm.create({}, 'AppView', AppView);
-  appView.render();
-  Router.initialize({appView: appView});  // The router now has a copy of all main appview
+  'jquery',
+  'collections/cels',
+  'views/sudoku',
+  'libs/hack/jquery'
+  
+], function($, Cels, Sudoku){
+	//create all cels
+	sudoku = new Sudoku({
+		collection: Cels
+	});
+	
+	
+	
 });
